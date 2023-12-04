b9024f06ffc9512892bbc71562e9609bcdeca08c
Brianna Van Hecke <vanheckeb@merrimack.edu>
2023-12-04 15:45:50 -0500

    added routing

--- merrimackcspage/src/App.js
+++ merrimackcspage/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
+import Navbar from "./components/Navbar";
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+// Routing Reference: https://www.geeksforgeeks.org/how-to-create-a-multi-page-website-using-react-js/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+
+      </Routes>
+    </Router>
   );
 }
 
---
+++ merrimackcspage/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <a href='/'>home</a> <br></br>
+      <a href='/blog'>blog</a>
+    </div>
+  )
+}
+
+export default Navbar
