b54f8034f7f44aa1b024b93ea063a60365c3602b
Tonya E <luis1944alex@gmail.com>
2023-12-12 12:47:33 -0500

    Working on search bar.

--- merrimackcspage/src/pages/Knowledgebase.js
+++ merrimackcspage/src/pages/Knowledgebase.js
@@ -1,18 +1,50 @@
-import React from 'react'
-import { Container } from "react-bootstrap";
+import { React, useEffect, useState } from 'react'
 import CreatePostForm from "../components/CreatePostForm";
 import Post from "../components/Post"
+import { Autocomplete } from '@mantine/core';
+import { app, database } from './firebaseConfig';
+import { getDatabase, ref, onValue, push, child, update, set} from "firebase/database";
 
 function Knowledgebase() {
+
+  const [search, setSearch] = useState('');
+
+  var titles = [];
+
+  useEffect(() => {
+      titles = [];
+       // Get all of the titles and append them to titles
+       try {
+        const db = database;
+  
+        // Construct the path
+        const path = `knowledgebase`;
+
+        onValue(ref(db, path), async (snapshot) => {
+            snapshot.forEach(child => {
+              titles.push(child.key);
+            });
+        });
+
+      } catch (error) {
+          console.log(error);
+      }
+  });
+
   return (
 
     <div className='knowledgebase'>
 
-      <CreatePostForm></CreatePostForm>
-      
+      {/* Autocomplete tab for searching data */}
+      <div className='search'>
+        <Autocomplete label="Search" data={titles} value={search} onChange={setSearch} />
+      </div>
+
       <div className='posts' style={{ paddingTop: '60px', height: 'fit-content' }}>
           <Post postID={null} userID={null} userEmail={"elguezabala@merrimack.edu"} createTime={null} updateTime={null} initValue={'<p># Hello *world* \n this is line two</p>'} title="Example Post" tags={null} resources={null} comments={null} />  
       </div>
+
+      <CreatePostForm></CreatePostForm>
 
     </div>
 
